241ea8ac1ebd1cf0dd8be8541a11892cf017f590
Vishal Srivastav <42443221+sapjv@users.noreply.github.com>
2019-12-13 11:55:44 +0530

    Update Square root Decomposition ( Range Sum Query ) C++.cpp

--- Others ( Mixed )/Square root Decomposition ( Range Sum Query ) C++.cpp
+++ Others ( Mixed )/Square root Decomposition ( Range Sum Query ) C++.cpp
@@ -7,12 +7,12 @@ competitive programmers. This technique helps us to reduce Time Complexity by a 
 
 The key concept of this technique is to decompose given array into small chunks specifically of size sqrt(n).
 
-Let’s say we have an array of n elements and we decompose this array into small chunks of size sqrt(n). We will 
+Letâ€™s say we have an array of n elements and we decompose this array into small chunks of size sqrt(n). We will 
 be having exactly sqrt(n) such chunks provided that n is a perfect square. Therefore, now our array on n elements 
 is decomposed into sqrt(n) blocks, where each block contains sqrt(n) elements (assuming size of array is perfect 
 square).
 
-Let’s consider these chunks or blocks as an individual array each of which contains sqrt(n) elements and you have 
+Letâ€™s consider these chunks or blocks as an individual array each of which contains sqrt(n) elements and you have 
 computed your desired answer(according to your problem) individually for all the chunks. Now, you need to answer 
 certain queries asking you the answer for the elements in range l to r(l and r are starting and ending indices of 
 the array) in the original n sized array.
@@ -40,10 +40,10 @@ perfect square). In this case all the blocks are completely overlapped by our qu
 this query we need to iterate over all the decomposed blocks for the array and we know that the number of 
 blocks = sqrt(n). Hence, the complexity for this type of query will be O(sqrt(n)) in worst case.
 
-Note : Let’s consider a query [l = 1 and r = n-2] (n is the size of the array and has a 0 based indexing). Therefore, 
-for this query exactly ( sqrt(n) – 2 ) blocks will be completely overlapped where as the first and last block will be 
+Note : Letâ€™s consider a query [l = 1 and r = n-2] (n is the size of the array and has a 0 based indexing). Therefore, 
+for this query exactly ( sqrt(n) â€“ 2 ) blocks will be completely overlapped where as the first and last block will be 
 partially overlapped with just one element left outside the overlapping range. So, the completely overlapped blocks 
-can be summed up in ( sqrt(n) – 2 ) ~ sqrt(n) iterations, whereas first and last block are needed to be traversed 
+can be summed up in ( sqrt(n) â€“ 2 ) ~ sqrt(n) iterations, whereas first and last block are needed to be traversed 
 one by one separately. But as we know that the number of elements in each block is at max sqrt(n), to sum up last 
 block individually we need to make, (sqrt(n)-1) ~ sqrt(n) iterations and same for the last block.
 
@@ -51,8 +51,8 @@ So, the overall Complexity = O(sqrt(n)) + O(sqrt(n)) + O(sqrt(n)) = O(3*sqrt(N))
 
 
 Update Queries(Point update) : In this query we simply find the block in which the given index lies, then subtract 
-							   its previous value and add the new updated value as per the point update query.
- 							   So, the Time Complexity for point update is : O(1)
+			       its previous value and add the new updated value as per the point update query.
+ 			       So, the Time Complexity for point update is : O(1)
 
 */
 
