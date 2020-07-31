c844157902157a6ed01a14053488de86a2d3b922
Vishal Srivastav <42443221+sapjv@users.noreply.github.com>
2020-07-31 21:30:52 +0530

    Update Solution with O(n) time and O(n) space.cpp

--- Interview Problems/Trapping Rain Water Problem/Solution with O(n) time and O(n) space.cpp
+++ Interview Problems/Trapping Rain Water Problem/Solution with O(n) time and O(n) space.cpp
@@ -5,7 +5,7 @@
 Problem Link :  https://practice.geeksforgeeks.org/problems/trapping-rain-water/0
 
 Problem Statement : Given n non-negative integers representing an elevation map where the width 
-					          of each bar is 1, compute how much water it is able to trap after raining.
+of each bar is 1, compute how much water it is able to trap after raining.
 
 Example Image Link :  https://www.geeksforgeeks.org/wp-content/uploads/watertrap.png
 
@@ -19,17 +19,17 @@ Algorithm:
 Step1 : Create two array left and right of size n. create a variable max_ = INT_MIN.
 
 Step2 : Run one loop from start to end. In each iteration update max_ as max_ = max(max_, arr[i]) 
-		    and also assign Lmax[i] = max_
+	and also assign Lmax[i] = max_
 
 Step3 : Update max_ = INT_MIN.
 
 Step4 : Run another loop from end to start. In each iteration update max_ as max_ = max(max_, arr[i]) 
-		    and also assign Rmax[i] = max_
+	and also assign Rmax[i] = max_
 
 Step5 : Traverse the array from start to end.
 
 Step6 : The amount of water that will be stored in this column is min(a,b) – array[i],(where a = Lmax[i] 
-		    and b = Rmax[i]) add this value to total amount of water stored.
+	and b = Rmax[i]) add this value to total amount of water stored.
 
 Step7 : Print the total amount of water stored.
 	
@@ -57,23 +57,23 @@ int main()
         for(int i=0;i<n;i++)
             cin>>a[i];
         
-		// Lmax[i] contains height of tallest bar to the left of ith bar including itself 
-		// Rmax[i] contains height of tallest bar to the right of ith bar including itself 
+	// Lmax[i] contains height of tallest bar to the left of ith bar including itself 
+	// Rmax[i] contains height of tallest bar to the right of ith bar including itself 
         vector<lli> Lmax(n),Rmax(n);
         
-		// Fill left array 
+	// Fill left array 
         Lmax[0] = a[0];
         for(int i=1;i<n;i++)
             Lmax[i] = max(a[i],Lmax[i-1]);
         
-		// Fill right array 
+	// Fill right array 
         Rmax[n-1] = a[n-1];
         for(int i=n-2;i>=0;i--)
             Rmax[i] = max(a[i],Rmax[i+1]);
         
-		// Calculate the accumulated water element by element. Consider the amount of water 
-		// on i'th bar, the amount of water accumulated on this particular bar will be equal 
-		// to min(left[i], right[i]) - arr[i] . 
+	// Calculate the accumulated water element by element. Consider the amount of water 
+	// on i'th bar, the amount of water accumulated on this particular bar will be equal 
+	// to min(left[i], right[i]) - arr[i] . 
         lli ans = 0;
         for(int i=0;i<n;i++)
         {
