3a88dc3d59b4b7ba925e37f5c4510b1be9f5158e
Vishal Srivastav <42443221+sapjv@users.noreply.github.com>
2020-09-22 11:19:44 +0530

    Create Return something from children of current node using DFS.cpp

---
+++ Tree/DFS/Return something from children of current node using DFS.cpp
@@ -0,0 +1,35 @@
+
+/*
+
+      ###  Return something from children of current node using DFS ### 
+
+If you really want to return something from bottom nodes to upper nodes i.e., from leaves to root node 
+using DFS ( and something you want to get from children of the current node so that you could modify 
+them for current node ) , then you have to use this technique : 
+
+for more information, check out my submission for this problem : 
+link :  http://codeforces.com/contest/839/submission/93452517
+
+*/
+
+vector<int> sum(M,0);
+
+void dfs(int source)
+{
+   visited[source] = true;
+   for(int x:adjList[source])
+   {
+      if(!visited[x])
+      {
+         dfs(x);
+         sum[source] += sum[x];			// here, we are adding all sums in children nodes of the current node
+      }
+   }
+   
+   // here we can do some modifications ( perform some operations ) on sum[source] like this :
+   sum[source] *= 5;
+}
+
+
+
+
